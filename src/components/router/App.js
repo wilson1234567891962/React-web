c715b8eed3fec12355ca67ed43bd12f16f47ec79
Alejandro gonzalez <wilson1234567891962@hotmail.com>
2019-05-25 12:56:57 -0500

    Implements page home

--- src/components/router/App.js
+++ src/components/router/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Layout from '../js/Layout';
-import Badges from '../../pages/Badges';
-import BadgeNew from '../../pages/BadgeNew';
-import NotFound from '../../pages/NotFound';
+import Layout from '../js/Layout.js';
+import Badges from '../../pages/Badges.js';
+import BadgeNew from '../../pages/BadgeNew.js';
+import NotFound from '../../pages/NotFound.js';
+import Home from '../../pages/Home.js';
 
 function App() {
     return (
         <BrowserRouter>
             <Layout>
                 <Switch>
+                    <Route exact path="/" component={Home} />
                     <Route exact path="/badges" component={BadgeNew} />
                     <Route exact path="/badges/new" component={Badges} />
                     <Route component={NotFound} />
